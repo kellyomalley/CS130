db7533269878762139b027b7af802dc06e2e3917
kellyomalley <kellymarieomalley@gmail.com>
2016-10-23 21:18:44 -0700

    Start of bet class
    Trying to figure out Swift

--- BoredBets/BoredBets/Bet.swift
+++ BoredBets/BoredBets/Bet.swift
@@ -9,3 +9,36 @@
 import Foundation
 
 
+// need to figure out how to refactor to make this fit a design pattern
+      //bets can be contructed passing in a bet id, bet title, bet description, and bet type
+      //description is optional
+      
+
+      //maybe template method?
+      
+      class Bet {
+        var id: Int
+        var title: String
+        var description: String = ""
+        var type: String
+
+        //no description
+        init(id: Int, title: String, type: String) {
+            self.id = id
+            self.title = title
+            self.type = type
+        }
+        
+        // description
+        init(id: Int, title: String, description: String, type:String) {
+            self.id = id
+            self.title = title
+            self.description = description
+            self.type = type
+        }
+       
+        //should always be overridden
+        func calculateOdds() -> String{
+                preconditionFailure()
+        }
+      }
