b9b54156a66f65930e22031436ee9fbecde0931d
kellyomalley <kellymarieomalley@gmail.com>
2016-10-23 17:36:48 -0700

    Project Created

---
+++ BoredBets/BoredBets/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  BoredBets
+//
+//  Created by Kelly O'Malley on 10/23/16.
+//  Copyright © 2016 Kelly O'Malley. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
