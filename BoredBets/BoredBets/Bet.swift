2b7919ae53f952e7ee0d7bb37504b9153e8fa335
Sam Sobell <sam.sobell@gmail.com>
2016-10-23 19:43:40 -0700

    important changes

--- BoredBets/BoredBets/Bet.swift
+++ BoredBets/BoredBets/Bet.swift
@@ -1,9 +1,9 @@
-//
-//  Bet.swift
-//  BoredBets
-//
-//  Created by Markus Notti on 10/23/16.
-//  Copyright © 2016 SauceKitchen. All rights reserved.
+      //
+     //  Bet.swift
+    //  BoredBets
+   //
+  //  Created by Markus Notti on 10/23/16.
+ //  Copyright © 2016 SauceKitchen. All rights reserved.
 //
 
 import Foundation
