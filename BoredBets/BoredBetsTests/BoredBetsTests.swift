694bb904364cdf30688b62b1a2bfcd77d262e24d
kellyomalley <kellymarieomalley@gmail.com>
2016-11-02 19:53:40 -0700

    Some tests that won't compile because of Firebase

--- BoredBets/BoredBetsTests/BoredBetsTests.swift
+++ BoredBets/BoredBetsTests/BoredBetsTests.swift
@@ -6,31 +6,51 @@
 //  Copyright © 2016 SauceKitchen. All rights reserved.
 //
 
+import Foundation
 import XCTest
+
 @testable import BoredBets
 
 class BoredBetsTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let myTestBet = YesNoBet(title: "Testing the bet class!")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testWagerInFirebase() {
+        let sampleUserId = "123456"
+        //user bets $1000 on yes
+        myTestClass.attachWager(userId: sampleUserId, betAmount: 1000, userBet: 1)
+        var id: String = ""
+        //Now pull all the wagers from Firebase
+        //There should only be one, made by sampleUserId
+        myTestClass.wagerIds(completion: {
+            wagerIds in
+            self.wagersForWagerIds(wagerIds: wagerIds, wagers: [], completion: {
+                wagers in
+                for wager in wagers{
+                    id = wager.getUser()
+                }
+            })
+        })
+        
+        //Assert that the one present is the one we found
+        XCTAssert(id == sampleUserId)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testOddsCalculation() {
+        //purposely made everyone bet yes so a simple division tries to divide by zero
+        myTestClass.attachWager(userId: "15234", betAmount: 2000, userBet: 1)
+        myTestClass.attachWager(userId: "65453", betAmount: 1000, userBet: 1)
+        let expectedResult = "Odds: 1:0, Pool: $3000"
+        let odds = myTestClass.calculateOdds()
+        
+        XCTAssert(expectedResult == odds)
     }
     
 }
